17b83241affdd88db74e7d4665a143efc8b7cbbe
Len Vo <lenvo789@gmail.com>
2024-06-20 16:43:08 +0700

    push baitap 2006

---
+++ src/vn/edu/likelion/study/app/Demo1.java
@@ -0,0 +1,19 @@
+package vn.edu.likelion.study.app;
+
+import vn.edu.likelion.study.model.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo1 {
+    public static void main(String[] args) {
+//        Person p1 = new Person();
+//        System.out.println(p1.getAge());
+//        p1.setAge(23);
+//        System.out.println(p1.getAge());
+//        System.out.println(p1.getName());
+//        p1.setName("Len");
+//        System.out.println(p1.getName());`
+        List<String> arrList = new ArrayList<>();
+    }
+}
